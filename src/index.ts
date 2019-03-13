e8caf00975616301805e2333cbf5d30b0df7241c
ryanpetersonOF <ryan@openfin.co>
2019-03-13 21:33:58 +0000

    Add CLI options

--- src/index.ts
+++ src/index.ts
@@ -44,12 +44,35 @@ export interface CLIArguments {
     writeToDisk: boolean;
 }
 
-const testArgs:CLIArguments = {
-    writeToDisk: false,
-    static: false,
-    launchApp: false,
-    mode: "development",
-    providerVersion: "local"
-};
+/**
+ * Starts the build + server process, passing in any provided CLI arguments.
+ * @param args 
+ */
+function startCommandProcess(args: CLIArguments & {launch: boolean}) {
+    const sanitizedArgs: CLIArguments = {
+        providerVersion: args.providerVersion || 'local',
+        mode: args.mode || 'development',
+        launchApp: args.launch || true,
+        static: args.static || false,
+        writeToDisk: args.writeToDisk || false
+    };
 
-startServer(testArgs);
+    startServer(sanitizedArgs);
+
+}
+
+program
+    .command('start', 'Starts your project by building and running on a local server')
+    .option('-v, --providerVersion <version>', 'Sets the runtime version for the provider.  Defaults to "local".', 'local')
+    .option('-m, --mode <mode>', 'Sets the webpack mode.  Defaults to "development".', 'development')
+    .option('-l, --launch', 'Launches the server and application once built.  Defaults to true.', true)
+    .option('-s, --static', 'Launches the server and application using pre-built files.  Defaults to false.', false)
+    .option('-w, --write', 'Writes and serves the built files from disk.  Defaults to false.', false)
+    .action(startCommandProcess);
+
+program.parse(process.argv);
+
+// if program was called with no arguments, show help.
+if (program.args.length === 0) {
+    program.help();
+}
