347ca7c6c2c97a77a6688acdf28f433e7ae74ff2
ryanpetersonOF <ryan@openfin.co>
2019-04-12 13:57:05 +0100

    [NA] Update copy dir

--- src/fileCopy.js
+++ src/fileCopy.js
@@ -1,11 +1,9 @@
 /**
  * Helper build utility to copy non-built files to the dist directory.  This itself file is not included in the distributed project.
  */
-
-const fs = require('fs');
 const path = require('path');
 
-const mkdirp = require('mkdirp');
+const fs = require('fs-extra');
 const glob = require('glob');
 
 // List of files to copy into the dist directory on build
@@ -17,13 +15,6 @@ const glob = require('glob');
 
 // List of directories to copy into the dist directory on build
 [
-    ...glob.sync('src/typedoc-template/**/*')
-].forEach(file => {
-    const isDir = fs.lstatSync(file).isDirectory();
-    if (isDir) {
-        fs.mkdirSync(path.dirname(path.resolve('dist', path.relative('./src', file))));
-    } else {
-        fs.copyFileSync(path.resolve(file), path.resolve('dist', path.relative('./src', file)));
-    }
-});
+    'src/typedoc-template'
+].forEach(file => fs.copySync(file, path.resolve('dist', path.relative('./src', file))));
 
