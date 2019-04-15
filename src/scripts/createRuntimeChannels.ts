5ac24734dfd6e28d7794770858a0b050af0c8d7d
pjbroadbent <p.j.broadbent@gmail.com>
2019-04-15 14:52:05 +0100

    [SERVICE-398] Runtime channels fix (#30)

--- src/scripts/createRuntimeChannels.ts
+++ src/scripts/createRuntimeChannels.ts
@@ -1,4 +1,4 @@
-import {writeFileSync} from 'fs';
+import {writeFileSync, existsSync} from 'fs';
 import {resolve} from 'path';
 
 import {getRootDirectory} from '../utils/getRootDirectory';
@@ -8,14 +8,26 @@ const RUNTIME_CHANNELS = ['stable', 'alpha', 'beta', 'canary'];
 /**
  * Creates copies of the provider's app.json file, that each point at a different runtime release channel.
  */
-export function createRuntimeChannels() {
-    const rootDir = getRootDirectory();
-    const manifest = require(resolve(rootDir, 'res/provider/app.json'));
+export function createRuntimeChannels(): void {
+    const manifest = require(getProviderManifestPath());
 
     RUNTIME_CHANNELS.forEach(channel => {
         manifest.runtime.version = channel;
 
+        const rootDir = getRootDirectory();
         const output = JSON.stringify(manifest, null, 4);
         writeFileSync(resolve(rootDir, `dist/provider/app.runtime-${channel}.json`), output, {encoding: 'utf8'});
     });
+}
+
+function getProviderManifestPath(): string {
+    const rootDir = getRootDirectory();
+    const distPath = resolve(rootDir, 'dist/provider/app.json');
+    const resPath = resolve(rootDir, 'res/provider/app.json');
+
+    if (existsSync(distPath)) {
+        return distPath;
+    } else {
+        return resPath;
+    }
 }
