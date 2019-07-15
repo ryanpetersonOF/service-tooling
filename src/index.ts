5812da1d9b814979bf46cb4b9018be3134be6bf8
ryanpetersonOF <ryan@openfin.co>
2019-07-15 15:34:30 +0100

    [SERVICE-531] Base import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import {createAppJsonMiddleware, createCustomManifestMiddleware} from './server/middleware';
+import * as spawn from './testing/tools/spawn';
 import * as webpackTools from './webpack/webpackTools';
 
 export = {
     webpackTools,
-    middleware: {createCustomManifestMiddleware, createAppJsonMiddleware}
+    middleware: {createCustomManifestMiddleware, createAppJsonMiddleware},
+    testTools: {
+        spawn: {...spawn}
+    }
 };
