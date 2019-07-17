0edac755b74b4bf15e9eebcd1299074940ce208d
ryanpetersonOF <ryan@openfin.co>
2019-07-17 14:16:35 +0100

    [SERVICE-531] Cleanup top level

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import {createAppJsonMiddleware, createCustomManifestMiddleware} from './server/middleware';
-import * as spawn from './testing/tools/spawn';
 import * as webpackTools from './webpack/webpackTools';
 
-export = {
-    webpackTools,
-    middleware: {createCustomManifestMiddleware, createAppJsonMiddleware},
-    testTools: {
-        spawn: {...spawn}
-    }
-};
+export const middleware = {createCustomManifestMiddleware, createAppJsonMiddleware};
+export {webpackTools};
